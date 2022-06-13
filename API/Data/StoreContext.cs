23bd4ecc26a5b674428f36c7343dcd5dfdf4af91
rengetsu <rengetsu@mail.ru>
2022-06-13 23:44:18 +0300

    DbContext

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,6 +1,14 @@
-﻿namespace API.Data
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
 {
-    public class StoreContext
+    public class StoreContext : DbContext
     {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> MyProperty { get; set; }
     }
 }
