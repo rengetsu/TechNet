b2eb900d84f96569e330a0cfdc7f71f3de484be8
rengetsu <rengetsu@mail.ru>
2022-06-30 23:17:13 +0300

    Add some comments

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -5,10 +5,17 @@ namespace API.Data
 {
     public class StoreContext : DbContext
     {
+        /// <summary>
+        /// Store Context
+        /// </summary>
+        /// <param name="options"></param>
         public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
         }
 
+        /// <summary>
+        /// Database set for products
+        /// </summary>
         public DbSet<Product> Products { get; set; }
     }
 }
