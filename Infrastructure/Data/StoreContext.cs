3b6935f34f4bd45db3de9e816443ffc05e5b6413
rengetsu <rengetsu@mail.ru>
2023-01-07 20:38:55 +0200

    Move Data to Infrastructure

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,21 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        /// <summary>
+        /// Store Context
+        /// </summary>
+        /// <param name="options"></param>
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Database set for products
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
+    }
+}
